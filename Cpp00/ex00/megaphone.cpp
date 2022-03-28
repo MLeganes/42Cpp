1a5c8f00300235578f0daf10a9651f5f6b0be914
MLeganes <a.eme.ele@gmail.com>
2022-03-28 15:54:34 +0200

    megaphone

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 
-int
-	main(int argc, char const *argv[])
+int	main(int argc, char **argv)
 {
+	std::string str;
+
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
-			std::string str(argv[i]);
+			str = argv[i];
 			for (size_t j = 0; j < str.length(); j++)
 				std::cout << (char)std::toupper(str[j]);
 		}
