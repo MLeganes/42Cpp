8e84d5fa2ecf6ea681039bab43539e66396897b2
Angel Morcillo leganes <amorcill@3-B-10.42heilbronn.de>
2022-05-27 13:19:24 +0200

    ex00 done

--- Cpp01/ex00/main.cpp
+++ Cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 11:57:16 by amorcill          #+#    #+#             */
-/*   Updated: 2022/05/25 21:38:42 by amorcill         ###   ########.fr       */
+/*   Updated: 2022/05/27 13:17:43 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,11 @@ int main(void){
 	Zombie *zmb1;
 	zmb1 = newZombie("Zombie1");
 	zmb1->announce();
-	delete zmb1;	
+	delete zmb1;
 
 	// 2. Example 
 	std::cout << "\n2. Example" << std::endl;
 	randomChump("Zombie2");
 	
-	system("leaks Zombie");
 	return (0);
 }
