97808544484c8c4c5fbdbd9925ac265d08faeb04
MLeganes <a.eme.ele@gmail.com>
2022-05-31 01:07:54 +0200

    ex04 done

--- Cpp01/ex04/main.cpp
+++ Cpp01/ex04/main.cpp
@@ -1,25 +1,55 @@
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-#include "Weapon.hpp"
-#include <stdlib.h>
-#include <unistd.h>
 
-int main()
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char **argv)
 {
-	{
-		Weapon club = Weapon("crude spiked club");
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
+	(void)argv;
+	if (argc != 4) {
+		std::cout << "Error: wrong number of arguments" << std::endl;
+		std::cout << "Example of use: ./sed [filename] [string1] [string2] " << std::endl;
+		return (1);
 	}
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	// Open input file
+	std::ifstream ifs(filename.c_str());
+	if (!ifs.is_open())
 	{
-		Weapon club = Weapon("crude spiked club");
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
+		std::cout << "Error: file \"" << argv[1] << "\" can not be opened "  << std::endl;
+		return (1);
 	}
-	return 0;
+	
+	// Open output file
+	std::string filename_output = filename + ".replace";
+	std::ofstream ofs(filename_output.c_str());
+	if (!ofs.is_open())
+	{
+		std::cout << "Error: file \"" << argv[1] << "\" can not be opened "  << std::endl;
+		return (1);
+	}
+
+	// Copy in buffer input file.
+	std::stringstream buffer;
+	buffer << ifs.rdbuf();
+	ifs.close();
+
+	// Init string with file input.
+	std::string str =  buffer.str();
+
+	// Start the find and replace.
+	for(size_t pos = str.find(s1); pos != std::string::npos; pos = str.find(s1, pos + s1.length()) )
+	{
+		str.erase(pos, s1.length());
+		str.insert(pos, s2);
+	}
+
+	// Save the new-replace in output file.
+	ofs << str;
+	ofs.close();
+	return (0);
 }
