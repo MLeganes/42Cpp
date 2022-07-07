7e2f405c9e5f5e2e4b07905e12be140a280db250
Angel Morcillo leganes <amorcill@1-B-4.42heilbronn.de>
2022-07-07 14:32:51 +0200

    Cpp02

---
+++ Cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,22 @@
+#ifndef MUTANTSTACK_HPP
+#define	MUTANTSTACK_HPP
+
+class MutantStack
+{
+private:
+	/* data */
+public:
+	MutantStack(/* args */);
+	~MutantStack();
+};
+
+MutantStack::MutantStack(/* args */)
+{
+}
+
+MutantStack::~MutantStack()
+{
+}
+
+
+#endif
