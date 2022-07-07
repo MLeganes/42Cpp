624fa60016b4b1596bca0a953a5808a95bf07cdf
Angel Morcillo leganes <amorcill@1-B-4.42heilbronn.de>
2022-07-07 16:15:45 +0200

    Cpp08

--- Cpp08/ex02/MutantStack.hpp
+++ Cpp08/ex02/MutantStack.hpp
@@ -1,22 +1,37 @@
 #ifndef MUTANTSTACK_HPP
 #define	MUTANTSTACK_HPP
 
-class MutantStack
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
 {
 private:
-	/* data */
 public:
-	MutantStack(/* args */);
-	~MutantStack();
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack<T> &copy) : std::stack<T>(copy) {}
+	~MutantStack() {}
+
+	MutantStack &operator=(const MutantStack<T> &obj)
+	{
+		if (*this != obj)
+		{
+			this = obj;
+		}
+		return *this;
+	}
+
+	iterator begin()
+	{
+		// return (std::stack<T>::c.begin());
+		return (this->c.begin());
+	}
+	iterator end()
+	{
+		// return (std::stack<T>::c.end());
+		return (this->c.end());
+	}
 };
-
-MutantStack::MutantStack(/* args */)
-{
-}
-
-MutantStack::~MutantStack()
-{
-}
-
 
 #endif
